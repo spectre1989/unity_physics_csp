15cb9363584654b5fd37de77b8b601fa6e0f4179
Joe Best-Rotheray <joebestrotheray@gmail.com>
2018-04-02 00:03:49 +0100

    started adding gui

---
+++ Assets/Gui.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gui : MonoBehaviour
+{
+    public GameObject server_display_player;
+    public GameObject proxy_player;
+
+    public void OnToggleServerPlayer(bool enabled)
+    {
+        this.server_display_player.SetActive(enabled);
+    }
+
+    public void OnToggleProxyPlayer(bool enabled)
+    {
+        this.proxy_player.SetActive(enabled);
+    }
+}
