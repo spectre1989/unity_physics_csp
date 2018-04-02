4ffa060761717ee7a65b283b2efe43efefe3f7c1
Joe Best-Rotheray <joebestrotheray@gmail.com>
2018-04-03 00:07:29 +0100

    more gui work

--- Assets/Gui.cs
+++ Assets/Gui.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Gui : MonoBehaviour
 {
+    public Logic logic;
     public GameObject server_display_player;
     public GameObject proxy_player;
+    public Text packet_loss_label;
 
     public void OnToggleServerPlayer(bool enabled)
     {
@@ -15,5 +18,16 @@ public class Gui : MonoBehaviour
     public void OnToggleProxyPlayer(bool enabled)
     {
         this.proxy_player.SetActive(enabled);
+    }
+
+    public void OnToggleCorrections(bool enabled)
+    {
+        this.logic.enable_corrections = enabled;
+    }
+
+    public void OnPacketLossSliderChanged(float value)
+    {
+        this.packet_loss_label.text = string.Format("Packet Loss - {0:F1}%", value);
+        this.logic.packet_loss_chance = value;
     }
 }
--- Assets/Logic.cs
+++ Assets/Logic.cs
@@ -42,9 +42,9 @@ public class Logic : MonoBehaviour
     public GameObject server_player;
     public GameObject server_display_player;
     public GameObject proxy_player;
-    
-    public const float c_latency = 0.1f; // todo(jbr) make a variable to tweak
-    public const float c_packet_loss = 0.9f;
+    public bool enable_corrections = true;
+    public float latency = 0.1f;
+    public float packet_loss_chance = 0.05f;
 
     private const float c_max_prediction_error_pos = 0.1f;
     private const float c_max_prediction_error_pos_sq = c_max_prediction_error_pos * c_max_prediction_error_pos;
@@ -112,10 +112,10 @@ public class Logic : MonoBehaviour
                 dt);
             
             // send input packet to server
-            if (Random.value < Logic.c_packet_loss)
+            if (Random.value > this.packet_loss_chance)
             {
                 InputMessage input_msg;
-                input_msg.delivery_time = Time.time + Logic.c_latency;
+                input_msg.delivery_time = Time.time + this.latency;
                 input_msg.start_tick_number = this.client_last_received_state_tick;
                 input_msg.inputs = new List<Inputs>();
                 for (uint tick = this.client_last_received_state_tick; tick <= client_tick_number; ++tick)
@@ -141,26 +141,29 @@ public class Logic : MonoBehaviour
             this.proxy_player.transform.position = state_msg.position;
             this.proxy_player.transform.rotation = state_msg.rotation;
 
-            // if there's enough error between client&server, then rewind&replay
-            uint buffer_slot = state_msg.tick_number % c_client_buffer_size;
-            Vector3 position_error = state_msg.position - this.client_state_buffer[buffer_slot].position;
-            float rotation_error = Quaternion.Dot(state_msg.rotation, this.client_state_buffer[buffer_slot].rotation);
-            if (position_error.sqrMagnitude > c_max_prediction_error_pos_sq ||
-                rotation_error < c_max_prediction_error_rot_dot)
+            if (this.enable_corrections)
             {
-                Rigidbody rigidbody = this.client_player.GetComponent<Rigidbody>();
-                rigidbody.position = state_msg.position;
-                rigidbody.rotation = state_msg.rotation;
-                rigidbody.velocity = state_msg.velocity;
-                rigidbody.angularVelocity = state_msg.angular_velocity;
-
-                uint rewind_tick_number = state_msg.tick_number;
-                while (rewind_tick_number < client_tick_number)
+                // if there's enough error between client&server, then rewind&replay
+                uint buffer_slot = state_msg.tick_number % c_client_buffer_size;
+                Vector3 position_error = state_msg.position - this.client_state_buffer[buffer_slot].position;
+                float rotation_error = Quaternion.Dot(state_msg.rotation, this.client_state_buffer[buffer_slot].rotation);
+                if (position_error.sqrMagnitude > c_max_prediction_error_pos_sq ||
+                    rotation_error < c_max_prediction_error_rot_dot)
                 {
-                    buffer_slot = rewind_tick_number % c_client_buffer_size;
-                    this.ClientStoreCurrentStateAndStep(ref this.client_state_buffer[buffer_slot], rigidbody, this.client_input_buffer[buffer_slot], dt);
+                    Rigidbody rigidbody = this.client_player.GetComponent<Rigidbody>();
+                    rigidbody.position = state_msg.position;
+                    rigidbody.rotation = state_msg.rotation;
+                    rigidbody.velocity = state_msg.velocity;
+                    rigidbody.angularVelocity = state_msg.angular_velocity;
 
-                    ++rewind_tick_number;
+                    uint rewind_tick_number = state_msg.tick_number;
+                    while (rewind_tick_number < client_tick_number)
+                    {
+                        buffer_slot = rewind_tick_number % c_client_buffer_size;
+                        this.ClientStoreCurrentStateAndStep(ref this.client_state_buffer[buffer_slot], rigidbody, this.client_input_buffer[buffer_slot], dt);
+
+                        ++rewind_tick_number;
+                    }
                 }
             }
         }
@@ -203,10 +206,10 @@ public class Logic : MonoBehaviour
                     {
                         server_tick_accumulator = 0;
 
-                        if (Random.value < Logic.c_packet_loss)
+                        if (Random.value > this.packet_loss_chance)
                         {
                             StateMessage state_msg;
-                            state_msg.delivery_time = Time.time + Logic.c_latency;
+                            state_msg.delivery_time = Time.time + this.latency;
                             state_msg.tick_number = server_tick_number;
                             state_msg.position = server_rigidbody.position;
                             state_msg.rotation = server_rigidbody.rotation;
