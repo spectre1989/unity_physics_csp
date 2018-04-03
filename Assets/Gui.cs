9b0b7dc8f1d3dcc39d3108a4b2a118f0d56674dc
Joe Best-Rotheray <joebestrotheray@gmail.com>
2018-04-03 13:51:42 +0100

    finished gui

--- Assets/Gui.cs
+++ Assets/Gui.cs
@@ -8,7 +8,26 @@ public class Gui : MonoBehaviour
     public Logic logic;
     public GameObject server_display_player;
     public GameObject proxy_player;
+
+    public Toggle corrections_toggle;
+    public Toggle server_player_toggle;
+    public Toggle proxy_player_toggle;
+    public Slider packet_loss_slider;
     public Text packet_loss_label;
+    public Slider latency_slider;
+    public Text latency_label;
+    public Slider snapshot_rate_slider;
+    public Text snapshot_rate_label;
+
+    public void Start()
+    {
+        this.corrections_toggle.isOn = true;
+        this.server_player_toggle.isOn = false;
+        this.proxy_player_toggle.isOn = false;
+        this.packet_loss_slider.value = this.logic.packet_loss_chance;
+        this.latency_slider.value = this.logic.latency;
+        this.snapshot_rate_slider.value = Mathf.Log(this.logic.server_snapshot_rate, 2.0f);
+    }
 
     public void OnToggleServerPlayer(bool enabled)
     {
@@ -29,5 +48,18 @@ public class Gui : MonoBehaviour
     {
         this.packet_loss_label.text = string.Format("Packet Loss - {0:F1}%", value);
         this.logic.packet_loss_chance = value;
+    }
+
+    public void OnLatencySliderChanged(float value)
+    {
+        this.latency_label.text = string.Format("Latency - {0}ms", (int)(value * 1000.0f));
+        this.logic.latency = value;
+    }
+
+    public void OnSnapshotRateSliderChanged(float value)
+    {
+        uint rate = (uint)Mathf.Pow(2, value);
+        this.snapshot_rate_label.text = string.Format("Snapshot Rate - {0}hz", 64/rate);
+        this.logic.server_snapshot_rate = rate;
     }
 }
